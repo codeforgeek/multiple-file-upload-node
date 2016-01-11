cf5f8f217cdbc1042fcd965e37fb23eafd95c902
Shahid <shahid@codeoforgeek>
2016-01-11 14:23:30 +0530

    first commit

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 var express	=	require("express");
+var bodyParser =	require("body-parser");
 var multer	=	require('multer');
 var app	=	express();
+app.use(bodyParser.json());
 var storage	=	multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, './uploads');
@@ -9,7 +11,7 @@ var storage	=	multer.diskStorage({
     callback(null, file.fieldname + '-' + Date.now());
   }
 });
-var upload = multer({ storage : storage}).single('userPhoto');
+var upload = multer({ storage : storage }).array('userPhoto',2);
 
 app.get('/',function(req,res){
       res.sendFile(__dirname + "/index.html");
@@ -17,6 +19,8 @@ app.get('/',function(req,res){
 
 app.post('/api/photo',function(req,res){
 	upload(req,res,function(err) {
+		console.log(req.body);
+		console.log(req.files);
 		if(err) {
 			return res.end("Error uploading file.");
 		}
